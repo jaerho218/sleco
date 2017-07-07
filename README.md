e706c2534f6242cbcee2bcdba9e65f08b1fa2350
jaerho218 <ehoekstra@rogers.com>
2017-07-06 21:01:36 -0400

    Supervisor_Email_sending

---
+++ README.md
@@ -0,0 +1,3 @@
+Configure "emailsetup.ini" with SMTP details and you are good to go.
+Refer to "Supervisor_call.py" file for usage
+Revise the Machine and reason codes
---
+++ Supervisor_call.py
@@ -0,0 +1,32 @@
+##--- Eric Hoekstra
+##--- Date: 1st July 2017
+##--- Version: 1.01
+
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(17, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+from email_handler import Class_eMail
+
+#set the email ID the address to send the email 
+To_Email_ID = "erichoekstra218@gmail.com"
+
+
+while True:
+    inputValue = GPIO.input(17)
+    if (inputValue == False):
+        print("Button press ")
+        GPIO.output(18,GPIO.HIGH)
+        email = Class_eMail()
+        email.send_Text_Mail(To_Email_ID, 'Machine 617', 'Supervisor to Machine 617')
+        del email
+    time.sleep(10)
+    GPIO.output(18,GPIO.LOW)
+    
+
+

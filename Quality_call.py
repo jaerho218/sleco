942ec41f1fe71b212c909efea0477b88993c8fbb
jaerho218 <ehoekstra@rogers.com>
2017-09-11 16:44:57 -0400

    Update Quality_call.py
    Changed GPIO pins used
    Fixed error in code for LED delay (indented once more)
    Commented out Print line

--- Quality_call.py
+++ Quality_call.py
@@ -8,8 +8,8 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(16, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(19, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 from email_handler import Class_eMail
 
@@ -18,12 +18,12 @@ To_Email_ID = "slecoplasticsnotify@gmail.com"
 
 
 while True:
-    inputValue = GPIO.input(17)
+    inputValue = GPIO.input(19)
     if (inputValue == False):
-        print("Button press ")
-        GPIO.output(18,GPIO.HIGH)
+#        print("Button press ")
+        GPIO.output(16,GPIO.HIGH)
         email = Class_eMail()
         email.send_Text_Mail(To_Email_ID, 'Machine 53', 'Quality Auditor to Machine 53')
         del email
-    time.sleep(10)
-    GPIO.output(18,GPIO.LOW)
+        time.sleep(10)
+    GPIO.output(16,GPIO.LOW)
